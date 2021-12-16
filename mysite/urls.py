1fbdf610dbcec16cf1f5f6cc34757d56c3a8ec1c
dpbaia <diana.baiadori@gmail.com>
2021-12-16 17:34:52 +0100

    added models questions and answers, admin page and setup postgresql

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+"""
+    The idea behind include() is to make it easy to plug-and-play URLs. 
+    Since polls are in their own URLconf (polls/urls.py), they can be placed under “/polls/”, or under “/fun_polls/”, 
+    or under “/content/polls/”, or any other path root, and the app will still work.
+"""
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls')), # The include() function allows referencing other URLconfs.
+    path('admin/', admin.site.urls)
 ]
+
+"""
+    path() takes 4 arguments, 2 required: route and view, then kwargs and name
+    route: the URL pattern
+    view: when it finds the route, it calls the specified view witha  HttpRequest object as the first argument
+    kwargs: arbitrary keyword arguments
+    name: naming url makes it easier to refer to it 
+"""
