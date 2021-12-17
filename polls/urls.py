465a5d0db8f76d501671718aa1dca701757616c7
dpbaia <diana.baiadori@gmail.com>
2021-12-17 13:19:39 +0100

    worked on the views and shortcuts

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,15 @@ from django.urls import path
 
 from . import views
 
+#  Using angle brackets “captures” part of the URL and sends it as a keyword argument to the view function.
+# The colon (:) separates the converter and pattern name.
+
+
+# Need to do below as to differentiate if other apps have the same names 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name="detail"),
+    path('<int:question_id>/results/', views.results, name="results"),
+    path('<int:question_id>/vote/', views.vote, name="vote")
 ]
